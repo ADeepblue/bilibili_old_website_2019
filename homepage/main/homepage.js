68c981536f062b083a7ef392c330bf5becccdb42
ADeepblue <144508175+ADeepblue@users.noreply.github.com>
2026-06-05 13:47:38 +0800

    Refactor UserScript metadata and content

--- homepage/main/homepage.js
+++ homepage/main/homepage.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       深蓝之亘
 // @match        https://www.bilibili.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
 // @grant        none
